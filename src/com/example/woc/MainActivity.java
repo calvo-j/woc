9bc35023f7979ef8a9587c478d1094b7713c6017
trowavo <trowavo@yahoo.co.uk>
2015-01-26 12:46:17 -0500

    cal method added

--- src/com/example/woc/MainActivity.java
+++ src/com/example/woc/MainActivity.java
@@ -1,10 +1,7 @@
 package com.example.woc;
 
-import android.R.string;
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -47,67 +44,29 @@ public class MainActivity extends Activity {
 					public void onClick(View v) {
 						// TODO Auto-generated method stub
 						String a;
-						double weight;
-						int temp;
 						
 						//for 40
-						a = start.getText().toString();
-						weight = Double.parseDouble(a);
-						weight = (weight/100)*40;
-						temp= (int)weight;
-						temp = temp/5;
-						temp = temp*5;
-						a = Integer.toString(temp);
+						a = cal(start, 40);
 						p40.setText("5 reps of "+a);
 						
 						//for 50
-						a = start.getText().toString();
-						weight = Double.parseDouble(a);
-						weight = (weight/100)*50;
-						temp= (int)weight;
-						temp = temp/5;
-						temp = temp*5;
-						a = Integer.toString(temp);
+						a = cal(start, 50);
 						p50.setText("5 reps of "+a);
 				
 						//for 60
-						a = start.getText().toString();
-						weight = Double.parseDouble(a);
-						weight = (weight/100)*60;
-						temp= (int)weight;
-						temp = temp/5;
-						temp = temp*5;
-						a = Integer.toString(temp);
+						a = cal(start, 60);
 						p60.setText("3 reps of "+a);
 						
 						//for 75
-						a = start.getText().toString();
-						weight = Double.parseDouble(a);
-						weight = (weight/100)*75;
-						temp= (int)weight;
-						temp = temp/5;
-						temp = temp*5;
-						a = Integer.toString(temp);
+						a = cal(start, 75);
 						p75.setText("5 reps of "+a);
 						
 						//for 80
-						a = start.getText().toString();
-						weight = Double.parseDouble(a);
-						weight = (weight/100)*80;
-						temp= (int)weight;
-						temp = temp/5;
-						temp = temp*5;
-						a = Integer.toString(temp);
+						a = cal(start, 80);
 						p80.setText("5 reps of "+a);
 				
 						//for 85
-						a = start.getText().toString();
-						weight = Double.parseDouble(a);
-						weight = (weight/100)*85;
-						temp= (int)weight;
-						temp = temp/5;
-						temp = temp*5;
-						a = Integer.toString(temp);
+						a = cal(start, 85);
 						p85.setText("5 or more reps of "+a);
 						
 			
@@ -140,67 +99,29 @@ public class MainActivity extends Activity {
 					public void onClick(View v) {
 						// TODO Auto-generated method stub
 						String a;
-						double weight;
-						int temp;
 						
 						//for 40
-						a = start.getText().toString();
-						weight = Double.parseDouble(a);
-						weight = (weight/100)*40;
-						temp= (int)weight;
-						temp = temp/5;
-						temp = temp*5;
-						a = Integer.toString(temp);
+						a = cal(start, 40);
 						p40.setText("5 reps of "+a);
 						
 						//for 50
-						a = start.getText().toString();
-						weight = Double.parseDouble(a);
-						weight = (weight/100)*50;
-						temp= (int)weight;
-						temp = temp/5;
-						temp = temp*5;
-						a = Integer.toString(temp);
+						a = cal(start, 50);
 						p50.setText("5 reps of "+a);
 				
 						//for 60
-						a = start.getText().toString();
-						weight = Double.parseDouble(a);
-						weight = (weight/100)*60;
-						temp= (int)weight;
-						temp = temp/5;
-						temp = temp*5;
-						a = Integer.toString(temp);
+						a = cal(start, 60);
 						p60.setText("3 reps of "+a);
 						
 						//for 80
-						a = start.getText().toString();
-						weight = Double.parseDouble(a);
-						weight = (weight/100)*80;
-						temp= (int)weight;
-						temp = temp/5;
-						temp = temp*5;
-						a = Integer.toString(temp);
+						a = cal(start, 80);
 						p80.setText("3 reps of "+a);
 						
 						//for 85
-						a = start.getText().toString();
-						weight = Double.parseDouble(a);
-						weight = (weight/100)*50;
-						temp= (int)weight;
-						temp = temp/5;
-						temp = temp*5;
-						a = Integer.toString(temp);
+						a = cal(start, 85);
 						p85.setText("3 reps of "+a);
 				
 						//for 90
-						a = start.getText().toString();
-						weight = Double.parseDouble(a);
-						weight = (weight/100)*60;
-						temp= (int)weight;
-						temp = temp/5;
-						temp = temp*5;
-						a = Integer.toString(temp);
+						a = cal(start, 90);
 						p90.setText("3 or more reps of "+a);
 			
 					}
@@ -233,67 +154,29 @@ public class MainActivity extends Activity {
 					public void onClick(View v) {
 						// TODO Auto-generated method stub
 						String a;
-						double weight;
-						int temp;
 						
 						//for 40
-						a = start.getText().toString();
-						weight = Double.parseDouble(a);
-						weight = (weight/100)*40;
-						temp= (int)weight;
-						temp = temp/5;
-						temp = temp*5;
-						a = Integer.toString(temp);
+						a = cal(start, 40);
 						p40.setText("5 reps of "+a);
 						
 						//for 50
-						a = start.getText().toString();
-						weight = Double.parseDouble(a);
-						weight = (weight/100)*50;
-						temp= (int)weight;
-						temp = temp/5;
-						temp = temp*5;
-						a = Integer.toString(temp);
+						a = cal(start, 50);
 						p50.setText("5 reps of "+a);
 				
 						//for 60
-						a = start.getText().toString();
-						weight = Double.parseDouble(a);
-						weight = (weight/100)*60;
-						temp= (int)weight;
-						temp = temp/5;
-						temp = temp*5;
-						a = Integer.toString(temp);
+						a = cal(start, 60);
 						p60.setText("3 reps of "+a);
 						
 						//for 75
-						a = start.getText().toString();
-						weight = Double.parseDouble(a);
-						weight = (weight/100)*75;
-						temp= (int)weight;
-						temp = temp/5;
-						temp = temp*5;
-						a = Integer.toString(temp);
+						a = cal(start, 75);
 						p75.setText("5 reps of "+a);
 						
 						//for 85
-						a = start.getText().toString();
-						weight = Double.parseDouble(a);
-						weight = (weight/100)*85;
-						temp= (int)weight;
-						temp = temp/5;
-						temp = temp*5;
-						a = Integer.toString(temp);
+						a = cal(start, 85);
 						p85.setText("3 reps of "+a);
 				
 						//for 95
-						a = start.getText().toString();
-						weight = Double.parseDouble(a);
-						weight = (weight/100)*95;
-						temp= (int)weight;
-						temp = temp/5;
-						temp = temp*5;
-						a = Integer.toString(temp);
+						a = cal(start, 95);
 						p95.setText("1 or more reps of "+a);
 			
 					}
@@ -325,67 +208,29 @@ public class MainActivity extends Activity {
 					public void onClick(View v) {
 						// TODO Auto-generated method stub
 						String a;
-						double weight;
-						int temp;
 						
 						//for 40
-						a = start.getText().toString();
-						weight = Double.parseDouble(a);
-						weight = (weight/100)*40;
-						temp= (int)weight;
-						temp = temp/5;
-						temp = temp*5;
-						a = Integer.toString(temp);
+						a = cal(start, 40);
 						p40.setText("5 reps of "+a);
 						
 						//for 50
-						a = start.getText().toString();
-						weight = Double.parseDouble(a);
-						weight = (weight/100)*50;
-						temp= (int)weight;
-						temp = temp/5;
-						temp = temp*5;
-						a = Integer.toString(temp);
+						a = cal(start, 50);
 						p50.setText("5 reps of "+a);
 				
 						//for 60
-						a = start.getText().toString();
-						weight = Double.parseDouble(a);
-						weight = (weight/100)*60;
-						temp= (int)weight;
-						temp = temp/5;
-						temp = temp*5;
-						a = Integer.toString(temp);
+						a = cal(start, 60);
 						p60.setText("3 reps of "+a);
 						
 						//for dd60
-						a = start.getText().toString();
-						weight = Double.parseDouble(a);
-						weight = (weight/100)*60;
-						temp= (int)weight;
-						temp = temp/5;
-						temp = temp*5;
-						a = Integer.toString(temp);
+						a = cal(start, 60);
 						pp60.setText("5 reps of "+a);
 						
 						//for 65
-						a = start.getText().toString();
-						weight = Double.parseDouble(a);
-						weight = (weight/100)*65;
-						temp= (int)weight;
-						temp = temp/5;
-						temp = temp*5;
-						a = Integer.toString(temp);
+						a = cal(start, 65);
 						p65.setText("5 reps of "+a);
 				
 						//for 70
-						a = start.getText().toString();
-						weight = Double.parseDouble(a);
-						weight = (weight/100)*70;
-						temp= (int)weight;
-						temp = temp/5;
-						temp = temp*5;
-						a = Integer.toString(temp);
+						a = cal(start, 70);
 						p70.setText("5 reps of "+a);
 						
 			
@@ -395,131 +240,23 @@ public class MainActivity extends Activity {
 			}
 		});
         
-        
-        
-        
-        
-        /*
-        Button butt = (Button) findViewById(R.id.Butt);
-        
-        final EditText start = (EditText) findViewById(R.id.editText1);
-        
-        final TextView p40 = (TextView) findViewById(R.id.p40);
-        final TextView p50 = (TextView) findViewById(R.id.p50);
-        final TextView p60 = (TextView) findViewById(R.id.p60);
-        final TextView p65 = (TextView) findViewById(R.id.p65);
-        final TextView p70 = (TextView) findViewById(R.id.p70);
-        final TextView p75 = (TextView) findViewById(R.id.p75);
-        final TextView p80 = (TextView) findViewById(R.id.p80);
-        final TextView p85 = (TextView) findViewById(R.id.p85);
-        final TextView p90 = (TextView) findViewById(R.id.p90);
-        final TextView p95 = (TextView) findViewById(R.id.p95);
-        
-        
-        butt.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				
-				String a;
-				double weight;
-				
-				//for 40
-				a = start.getText().toString();
-				weight = Double.parseDouble(a);
-				weight = (weight/100)*40;
-				weight = Math.round(weight *100);
-				weight = weight/100;
-				a = Double.toString(weight);
-				p40.setText("40% is "+a);
-				
-				//for 50
-				a = start.getText().toString();
-				weight = Double.parseDouble(a);
-				weight = (weight/100)*50;
-				weight = Math.round(weight *100);
-				weight = weight/100;
-				a = Double.toString(weight);
-				p50.setText("50% is "+a);
-				
-				//for 60
-				a = start.getText().toString();
-				weight = Double.parseDouble(a);
-				weight = (weight/100)*60;
-				weight = Math.round(weight *100);
-				weight = weight/100;
-				a = Double.toString(weight);
-				p60.setText("60% is "+a);
-				
-				//for 65
-				a = start.getText().toString();
-				weight = Double.parseDouble(a);
-				weight = (weight/100)*65;
-				weight = Math.round(weight *100);
-				weight = weight/100;
-				a = Double.toString(weight);
-				p65.setText("65% is "+a);
-				
-				//for 70
-				a = start.getText().toString();
-				weight = Double.parseDouble(a);
-				weight = (weight/100)*70;
-				weight = Math.round(weight *100);
-				weight = weight/100;
-				a = Double.toString(weight);
-				p70.setText("70% is "+a);
-				
-				//for 75
-				a = start.getText().toString();
-				weight = Double.parseDouble(a);
-				weight = (weight/100)*75;
-				weight = Math.round(weight *100);
-				weight = weight/100;
-				a = Double.toString(weight);
-				p75.setText("75% is "+a);
-				
-				//for 80
-				a = start.getText().toString();
-				weight = Double.parseDouble(a);
-				weight = (weight/100)*80;
-				weight = Math.round(weight *100);
-				weight = weight/100;
-				a = Double.toString(weight);
-				p80.setText("80% is "+a);
-				
-				//for 85
-				a = start.getText().toString();
-				weight = Double.parseDouble(a);
-				weight = (weight/100)*85;
-				weight = Math.round(weight *100);
-				weight = weight/100;
-				a = Double.toString(weight);
-				p85.setText("85% is "+a);
-				
-				//for 90
-				a = start.getText().toString();
-				weight = Double.parseDouble(a);
-				weight = (weight/100)*90;
-				weight = Math.round(weight *100);
-				weight = weight/100;
-				a = Double.toString(weight);
-				p90.setText("90% is "+a);
-				
-				//for 95
-				a = start.getText().toString();
-				weight = Double.parseDouble(a);
-				weight = (weight/100)*95;
-				weight = Math.round(weight *100);
-				weight = weight/100;
-				a = Double.toString(weight);
-				p95.setText("95% is "+a);
-				
-			}
-		});
-        
-       
-*/        
+    }
+    
+    public String cal(EditText start , int percent){
+    	
+    	int temp;
+    	double weight;
+    	String a;
+    	
+    	a = start.getText().toString();
+		weight = Double.parseDouble(a);
+		weight = (weight/100)*percent;
+		temp= (int)weight;
+		temp = temp/5;
+		temp = temp*5;
+		a = Integer.toString(temp);
+		return a; 
+    	
     }
 
 
